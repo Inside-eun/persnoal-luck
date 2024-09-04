7f4c1651c2e29658c8463f58d6c749bb8e2ebfec
inside-eun <dameun0808@gmail.com>
2024-09-04 17:28:15 +0900

    axios install

--- src/Component/BirthdayChanger.js
+++ src/Component/BirthdayChanger.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useRecoilState } from "recoil";
 import userGanji from "../State/UserGanji";
+import GetGanji from "../api/GetGanji";
 
 const BirthdayChanger = ({ birthday, time }) => {
   const [ganjiYear, setGanjiYear] = useState("");
@@ -10,65 +11,9 @@ const BirthdayChanger = ({ birthday, time }) => {
 
   const [ganji, setGanji] = useRecoilState(userGanji);
 
-  const heavenlyStems = [
-    "갑",
-    "을",
-    "병",
-    "정",
-    "무",
-    "기",
-    "경",
-    "신",
-    "임",
-    "계",
-  ];
-  const earthlyBranches = [
-    "자",
-    "축",
-    "인",
-    "묘",
-    "진",
-    "사",
-    "오",
-    "미",
-    "신",
-    "유",
-    "술",
-    "해",
-  ];
-
   useEffect(() => {
-    const [year, month, day] = birthday.split("-").map(Number);
-
-    const yearStemIndex = (year - 4) % 10;
-    const yearBranchIndex = (year - 4) % 12;
-    const newGanjiYear =
-      heavenlyStems[yearStemIndex] + earthlyBranches[yearBranchIndex];
-    setGanjiYear(newGanjiYear);
-
-    const monthBranchIndex = (month + 1) % 12;
-    const newGanjiMonth =
-      heavenlyStems[(yearStemIndex * 2 + month) % 10] +
-      earthlyBranches[monthBranchIndex];
-    setGanjiMonth(newGanjiMonth);
-
-    const baseDate = new Date(1900, 0, 1); // 1900년 1월 1일 기준
-    const targetDate = new Date(year, month - 1, day);
-    const diffDays = Math.floor(
-      (targetDate - baseDate) / (1000 * 60 * 60 * 24)
-    );
-    const dayStemIndex = (diffDays + 3) % 10; // 1900년 1월 1일은 무오일
-    const dayBranchIndex = (diffDays + 3) % 12;
-    const newGanjiDay =
-      heavenlyStems[dayStemIndex] + earthlyBranches[dayBranchIndex];
-    setGanjiDay(newGanjiDay);
-
-    const timeStems = time.substr(0, 1);
-    const timeBranchIndex =
-      heavenlyStems[earthlyBranches.indexOf(timeStems) % 10];
-    const newGanjiTime = timeStems + timeBranchIndex;
-    setGanjiTime(newGanjiTime);
-  }, [birthday, time]);
+    GetGanji();
+  }, []);
 
   useEffect(() => {
     setGanji((prev) => ({
